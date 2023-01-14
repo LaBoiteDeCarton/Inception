d627b50025bdccbea1f47a9770d3a521c4a0f1da
LaBoiteDeCarton <daniel.mercadiel@gmail.com>
2023-01-14 17:57:59 +0100

    env set correctly

--- srcs/requirements/mariadb/conf/config.sh
+++ srcs/requirements/mariadb/conf/config.sh
@@ -1,3 +1,4 @@
+echo "environnement :" $DATABASE_NAME $DATABASE_USER $DATABASE_PASSWORD
 if ! [ -d "/var/lib/mysql/wordpress" ]; then
 	service mysql start
 	mysql -u root -e "CREATE DATABASE $DATABASE_NAME; CREATE USER '$DATABASE_USER'@'%' IDENTIFIED BY '$DATABASE_PASSWORD'; GRANT ALL PRIVILEGES ON $DATABASE_NAME.* TO '$DATABASE_USER'@'%'; FLUSH PRIVILEGES;"
