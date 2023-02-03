e2acbcc20b40028d6c70c1ea75f80294cb93256f
LaBoiteDeCarton <daniel.mercadiel@gmail.com>
2023-02-03 15:10:16 +0100

    test

--- srcs/requirements/wordpress/conf/wordpress_config.sh
+++ srcs/requirements/wordpress/conf/wordpress_config.sh
@@ -5,7 +5,7 @@ if [ ! -e /var/www/wordpress/wp-config.php ]; then
 		--dbpass=$DATABASE_PASSWORD \
 	    --dbhost=mariadb:3306 --path='/var/www/wordpress'
 
-	wp core install --url=$DOMAIN_NAME --title='Blog Title' --admin_user=$ADMIN_NAME --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root --path='/var/www/wordpress'
+	wp core install --url=$DOMAIN_NAME --title=$SITE_NAME --admin_user=$ADMIN_NAME --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root --path='/var/www/wordpress'
 	wp user create --allow-root --role=author $USER_NAME $USER_MAIL --user_pass=$USER_PASSWORD --path='/var/www/wordpress' >> /log.txt
 fi
 
