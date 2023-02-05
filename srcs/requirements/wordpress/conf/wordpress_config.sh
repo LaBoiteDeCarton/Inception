f768d73cbeca85294781684bb06068f1454a55d4
LaBoiteDeCarton <daniel.mercadiel@gmail.com>
2023-02-05 13:35:01 +0100

    nginx reconf

--- srcs/requirements/wordpress/conf/wordpress_config.sh
+++ srcs/requirements/wordpress/conf/wordpress_config.sh
@@ -4,10 +4,20 @@ if [ ! -e /var/www/wordpress/wp-config.php ]; then
 		--dbname=$DATABASE_NAME \
 		--dbuser=$DATABASE_USER \
 		--dbpass=$DATABASE_PASSWORD \
-	    --dbhost=mariadb:3306 --path='/var/www/wordpress'
+	    --dbhost=mariadb:3306 \
+		--path='/var/www/wordpress'
 	sleep 10
-	wp core install --url=$DOMAIN_NAME --title=$SITE_NAME --admin_user=$ADMIN_NAME --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root --path='/var/www/wordpress'
-	wp user create --allow-root --role=author $USER_NAME $USER_MAIL --user_pass=$USER_PASSWORD --path='/var/www/wordpress' >> /log.txt
+	wp core install --url=$DOMAIN_NAME \
+		--title=$SITE_NAME \
+		--admin_user=$ADMIN_NAME \
+		--admin_password=$ADMIN_PASSWORD \
+		--admin_email=$ADMIN_EMAIL \
+		--allow-root \
+		--path='/var/www/wordpress'
+	wp user create --allow-root \
+		--role=author $USER_NAME $USER_MAIL \
+		--user_pass=$USER_PASSWORD \
+		--path='/var/www/wordpress' >> /log.txt
 fi
 
 if [ ! -d /run/php ]; then
